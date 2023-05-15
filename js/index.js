7e7e3770990bc5ab694294ca161f641ee7d16c2e
Kushagra Saxena <devkushagra2002@gmail.com>
2023-05-15 12:09:17 +0530

    Changes

--- js/index.js
+++ js/index.js
@@ -3,15 +3,14 @@ document.querySelector('a').onclick = function () {
 }
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCSS9UmY7JId8ygcwQbBnxhzmwGRd3B0sw",
-    authDomain: "elitmusassignment-5e9f3.firebaseapp.com",
-    databaseURL: "https://elitmusassignment-5e9f3-default-rtdb.firebaseio.com",
-    projectId: "elitmusassignment-5e9f3",
-    storageBucket: "elitmusassignment-5e9f3.appspot.com",
-    messagingSenderId: "437378714127",
-    appId: "1:437378714127:web:1c00da0abeb4bf4644b17a",
-    measurementId: "G-1JLKEE7JT0"
-};
+    apiKey: "AIzaSyBjrwRD89Pyp3JS06QrFzyitlvYSAAXaqw",
+    authDomain: "elitmusassign.firebaseapp.com",
+    databaseURL: "https://elitmusassign-default-rtdb.firebaseio.com/",
+    projectId: "elitmusassign",
+    storageBucket: "elitmusassign.appspot.com",
+    messagingSenderId: "177430761681",
+    appId: "1:177430761681:web:494ca3a3c44419578cf5f0"
+  };
 firebase.initializeApp(firebaseConfig);
 
 // Listen for form submit
